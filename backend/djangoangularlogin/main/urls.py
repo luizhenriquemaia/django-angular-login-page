08254a2084cc91dc6ba71c862ba41f34d25b125f
LuizHenriqueMaia <luizhenriquembh@gmail.com>
2021-08-18 01:53:33 -0300

    intial commit

---
+++ backend/djangoangularlogin/main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import LoginViewSet
+
+
+urlpatterns = [
+    path('api/auth/login', LoginViewSet.as_view()),
+]
---
+++ backend/djangoangularlogin/main/views.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from main.serializers import UserSerializer, LoginSerializer
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+
+class LoginViewSet(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        return Response({"user": UserSerializer(user,
+            context=self.get_serializer_context()).data, "message": "Login efetuado"},
+            status=status.HTTP_200_OK)
